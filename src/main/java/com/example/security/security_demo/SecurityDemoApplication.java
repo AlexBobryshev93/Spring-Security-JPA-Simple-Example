3ca16b12d2f7c4142ee29ec039f2a5cb2bf5d6ee
AlexBobryshev <darknessrider1@rambler.ru>
2019-10-23 16:32:41 +0300

    init dataloader added

--- src/main/java/com/example/security/security_demo/SecurityDemoApplication.java
+++ src/main/java/com/example/security/security_demo/SecurityDemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.security.security_demo;
 
+import com.example.security.security_demo.model.User;
+import com.example.security.security_demo.repo.UserRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SecurityDemoApplication {
@@ -10,4 +14,16 @@ public class SecurityDemoApplication {
         SpringApplication.run(SecurityDemoApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner dataLoader(UserRepo userRepo) {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                User user = new User();
+                user.setUsername("user");
+                user.setPassword("pass");
+                userRepo.save(user);
+            }
+        };
+    }
 }
